d1909949dbf403787c2ad21fa298846ecca78db6
Emanuel Millan <emanuelmillan455@gmail.com>
2022-12-03 07:39:07 -0500

    FEAT: se instala vuetify y axios, y se crea la vista principal del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-card class="mx-auto" max-width="600">
+        <v-toolbar dark color="#1f2137">
+        <v-toolbar-title class="mx-auto display-1"
+          >Sistema de gestion de ingreso</v-toolbar-title
+        >
+      </v-toolbar>
+      <v-form ref="form" lazy-validation class="my-3 mx-2" @submit.prevent="guardarIngreso">
+        <v-row>
+          <v-col cols="12" lg="12" sm="6">
+            <v-text-field
+              color="#1f2137"
+              prepend-icon="mdi-account"
+              v-model="codigo"
+              label="Codigo interno"
+            />
+          </v-col>
+          <v-btn
+            color="#1f2137"
+            style="color: white"
+            x-large
+            class="my-3 mx-auto"
+            type="submit"
+          >
+            <span>Ingresar</span>
+          </v-btn>
+        </v-row>
+      </v-form>  
+      </v-card>
+      
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  data: () => ({
+    codigo: ''
+  }),
+  methods: {
+    guardarIngreso() {
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    },
+  },
+};
+</script>
