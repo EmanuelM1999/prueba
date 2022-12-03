d6596fe2210e7785806f1fd699dfe82bc5204e13
Emanuel Millan <emanuelmillan455@gmail.com>
2022-12-03 07:43:11 -0500

    FEAT: se agrega la configuracion necesaria para poder trabajar con axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = "http://localhost:8000/api"
 
 new Vue({
   router,
